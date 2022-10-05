c75271c84b4c9384a9892977826bd633b5e51e1d
Alexandre Goloskok <alexandre@goloskok.com>
2022-10-05 14:36:12 +1000

    Add support for building with Carthage Make ABI stable

---
+++ OnlyPictures/OnlyPictures.h
@@ -0,0 +1,18 @@
+//
+//  OnlyPictures.h
+//  OnlyPictures
+//
+//  Created by alex on 5/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OnlyPictures.
+FOUNDATION_EXPORT double OnlyPicturesVersionNumber;
+
+//! Project version string for OnlyPictures.
+FOUNDATION_EXPORT const unsigned char OnlyPicturesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnlyPictures/PublicHeader.h>
+
+
